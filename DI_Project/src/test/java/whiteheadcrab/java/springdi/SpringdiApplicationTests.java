e93ae159892f48e60ce454717a155c3eed37500c
Whiteheadcrab <lettersorstudy@gmail.com>
2020-01-09 21:36:18 +0100

    Version 1.0 Initialized project

---
+++ DI_Project/src/test/java/whiteheadcrab/java/springdi/SpringdiApplicationTests.java
@@ -0,0 +1,13 @@
+package whiteheadcrab.java.springdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
