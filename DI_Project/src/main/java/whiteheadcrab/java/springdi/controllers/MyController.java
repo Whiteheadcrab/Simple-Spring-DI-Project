07dbf78809f051e431701d31ff8e011674977067
Whiteheadcrab <lettersorstudy@gmail.com>
2020-01-21 20:12:18 +0100

    Version 1.6.1 Added PrimaryGreetingService with @Primary annotation and changing MyController for using it

--- DI_Project/src/main/java/whiteheadcrab/java/springdi/controllers/MyController.java
+++ DI_Project/src/main/java/whiteheadcrab/java/springdi/controllers/MyController.java
@@ -1,10 +1,18 @@
 package whiteheadcrab.java.springdi.controllers;
 
 import org.springframework.stereotype.Controller;
+import whiteheadcrab.java.springdi.services.GreetingService;
 
 @Controller
 public class MyController
 {
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+
+        this.greetingService = greetingService;
+    }
+
     public String hello()
     {
         System.out.println("Hi!");
