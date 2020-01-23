fd6d434c7c76c6311385227bf6f155cdc78345a5
Whiteheadcrab <lettersorstudy@gmail.com>
2020-01-23 18:24:46 +0100

    Version 1.7.2 Added Spring Profile for English language , which is "en" and added Spring profile for Spanish , which is "es" . Activated profile "es"

--- DI_Project/src/main/java/whiteheadcrab/java/springdi/services/PrimaryGreetingService.java
+++ DI_Project/src/main/java/whiteheadcrab/java/springdi/services/PrimaryGreetingService.java
@@ -1,9 +1,11 @@
 package whiteheadcrab.java.springdi.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("en")
 @Primary
 public class PrimaryGreetingService implements GreetingService
 {
--- DI_Project/src/main/java/whiteheadcrab/java/springdi/services/PrimarySpanishGreetingService.java
+++ DI_Project/src/main/java/whiteheadcrab/java/springdi/services/PrimarySpanishGreetingService.java
@@ -1,8 +1,12 @@
 package whiteheadcrab.java.springdi.services;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("es")
+@Primary
 public class PrimarySpanishGreetingService implements  GreetingService
 {
     @Override
