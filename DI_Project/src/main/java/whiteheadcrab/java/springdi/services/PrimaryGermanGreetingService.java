18719e7c24ff9ad479fe0702c17d9b9cf5703b81
Whiteheadcrab <lettersorstudy@gmail.com>
2020-01-26 20:18:28 +0100

    Version 1.7.3 Added PrimaryGermanGreetingService for greetings on German

---
+++ DI_Project/src/main/java/whiteheadcrab/java/springdi/services/PrimaryGermanGreetingService.java
@@ -0,0 +1,17 @@
+package whiteheadcrab.java.springdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService
+{
+    @Override
+    public String sayGreeting()
+    {
+        return "Guten Tag!";
+    }
+}
