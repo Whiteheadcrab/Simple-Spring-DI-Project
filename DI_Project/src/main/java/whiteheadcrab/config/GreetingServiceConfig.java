5042442a8abbd5a0366f09366eb44bc03c1e0368
Whiteheadcrab <lettersorstudy@gmail.com>
2020-02-26 08:52:53 +0100

    Version 1.10.1 Added GreetingServiceFactory class for developing Configuration in this project . Initiated Configuration in class GreetingServiceConfig

---
+++ DI_Project/src/main/java/whiteheadcrab/config/GreetingServiceConfig.java
@@ -0,0 +1,10 @@
+package whiteheadcrab.config;
+
+import org.springframework.context.annotation.Configuration;
+import whiteheadcrab.services.GreetingService;
+
+@Configuration
+public class GreetingServiceConfig
+{
+
+}
