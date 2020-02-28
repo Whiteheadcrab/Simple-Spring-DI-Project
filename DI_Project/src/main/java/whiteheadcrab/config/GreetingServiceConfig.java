43aeff5661fd35bca6b3d189768dad8e4e92d5ab
Whiteheadcrab <lettersorstudy@gmail.com>
2020-02-28 16:24:19 +0100

    Version 1.11.1 Used Factory Beans for modifying program

--- DI_Project/src/main/java/whiteheadcrab/config/GreetingServiceConfig.java
+++ DI_Project/src/main/java/whiteheadcrab/config/GreetingServiceConfig.java
@@ -1,10 +1,42 @@
 package whiteheadcrab.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.graalvm.compiler.lir.CompositeValue;
+import org.springframework.context.annotation.*;
+import whiteheadcrab.services.GreetingRepository;
 import whiteheadcrab.services.GreetingService;
+import whiteheadcrab.services.GreetingServiceFactory;
 
 @Configuration
+@ComponentScan("whiteheadcrab.services")
 public class GreetingServiceConfig
 {
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository repository)
+    {
+        return new GreetingServiceFactory(repository);
+    }
 
+    @Bean
+    @Primary
+    @Profile({"default","en"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory)
+    {
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile({"es"})
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory)
+    {
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Primary
+    @Profile({"de"})
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory)
+    {
+        return greetingServiceFactory.createGreetingService("de");
+    }
 }
