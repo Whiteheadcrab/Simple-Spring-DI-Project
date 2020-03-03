1f8c464c26731d5da3f59779b6ef46c03544d4a9
Whiteheadcrab <lettersorstudy@gmail.com>
2020-03-03 09:39:59 +0100

    Version 1.13.3 Updated PropertyConfig for using FakeJmsBroker

--- DI_Project/src/main/java/whiteheadcrab/config/PropertyConfig.java
+++ DI_Project/src/main/java/whiteheadcrab/config/PropertyConfig.java
@@ -8,29 +8,49 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import whiteheadcrab.examplebean.FakeDataSource;
+import whiteheadcrab.examplebean.FakeJmsBroker;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
-public class PropertyConfig
-{
+public class PropertyConfig {
     @Autowired
     Environment env;
 
     @Value("${whiteheadcrab.username}")
     String user;
+
     @Value("${whiteheadcrab.password}")
     String password;
+
     @Value("${whiteheadcrab.dburl}")
     String url;
 
+    @Value("${whitehadcrab.jms.username}")
+    String jmsUsername;
+
+    @Value("${whitehadcrab.jms.password}")
+    String jmsPassword;
+
+    @Value("${whitheadcrab.jms.url}")
+    String jmsUrl;
+
     @Bean
-    public FakeDataSource fakeDataSource()
-    {
+    public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUsername(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker()
+    {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        return fakeJmsBroker;
     }
 
     @Bean
